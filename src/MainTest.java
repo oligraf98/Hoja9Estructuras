6aab2fe0b0361844e91cf78fef7249b28f583ae4
Oliver Graf <gra17190@uvg.edu.gt>
2019-05-17 19:07:16 -0600

    Junit y uml

---
+++ src/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void floydWarshall() {
+        Boolean x = true;
+        assertEquals(x,true);
+    }
+}
